00186d2951d31deb46f44fc234fc3e9e90888ee0
Ben Trinh <bjtrinh@ucdavis.edu>
2025-04-05 20:20:36 -0700

    add

---
+++ script.js
@@ -0,0 +1,54 @@
+(function(){
+    "use strict"
+    console.log('running js');
+
+    const button = document.querySelector('button'); //event listener, changer color, border, radius, font
+    const body = document.querySelector('body'); //change color, fonts
+    const header = document.querySelector('header'); //change background and text color, border radius
+    const main = document.querySelector('main'); //change font mayber
+    const section = document.querySelectorAll('section'); //hover efect
+    const h3 = document.querySelectorAll('h3'); //color
+    const li = document.querySelectorAll('ul li'); //border radius, hover
+    const lia = document.querySelectorAll('ul li a'); //color
+
+    let mode = 'blue';
+
+    button.addEventListener('click', function() {
+        if (mode === 'blue') {
+            body.className = 'switch';
+            button.className = 'switch';
+            header.className = 'switch';
+            main.className = 'switch';
+            for (let i = 0; i < section.length; i++) {
+                section[i].className = 'switch';
+            }
+            for (let i = 0; i < h3.length; i++) {
+                h3[i].className = 'switch';
+            }
+            for (let i = 0; i < li.length; i++) {
+                li[i].className = 'switch';
+            }for (let i = 0; i < lia.length; i++) {
+                lia[i].className = 'switch';
+            }
+
+            mode = 'orange';
+        } else {
+            body.removeAttribute('class');
+            button.removeAttribute('class');
+            header.removeAttribute('class');
+            main.removeAttribute('class');
+            for (let i = 0; i < section.length; i++) {
+                section[i].removeAttribute('class');
+            }for (let i = 0; i < h3.length; i++) {
+                h3[i].removeAttribute('class');
+            }
+            for (let i = 0; i < li.length; i++) {
+                li[i].removeAttribute('class');
+            }for (let i = 0; i < lia.length; i++) {
+                lia[i].removeAttribute('class');
+            }
+
+            mode = 'blue'
+        }
+    })
+})()
