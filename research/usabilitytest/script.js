4e454bc0d6adacb3a1984deb684750cc5b1bd078
Ben Trinh <bjtrinh@ucdavis.edu>
2025-05-29 08:33:34 -0700

    add

---
+++ research/usabilitytest/script.js
@@ -0,0 +1,11 @@
+(function(){
+    AOS.init({
+        duration: 1000,
+        once: false,
+        easing: 'ease'
+    });
+
+    window.addEventListener('load', function () {
+        alert("You are a first-year UC Davis student and you don't know much in depth information about cliamte change. You are pretty impartial to climate change: You think it is a real problem, but not a pressing one. You open this informative website to be more informed about the matter. ");
+    });
+})();
